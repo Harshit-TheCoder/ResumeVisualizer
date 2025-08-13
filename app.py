2aab3be7ebfdfdd056a4dc9541bb6cf65c21a15a
Harshit-TheCoder <harshitharlalka11@gmail.com>
2025-08-13 10:38:49 +0530

    sidebar part removed 2

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ st.title("Welcome to Resume Tool")
 st.write("Use the sidebar to navigate to Resume Optimizer or Resume Generator.")
 
 
-def show_sidebar():
-    st.sidebar.title("My Sidebar")
-    st.sidebar.write("")
-    st.sidebar.write("Resume_Optimizer.py")
-    st.sidebar.markdown("[Resume Generator](Resume_Generator.py)")
+# def show_sidebar():
+#     st.sidebar.title("My Sidebar")
+#     st.sidebar.write("")
+#     st.sidebar.write("Resume_Optimizer.py")
+#     st.sidebar.markdown("[Resume Generator](Resume_Generator.py)")
 
-show_sidebar()
+# show_sidebar()
